2c2d3f2ac2a1eb65bd6559b1a83eb18dd302851d
Luiz Grigio <lhgrigio@gmail.com>
2022-12-17 17:15:42 -0300

    fix(): requirements info

--- src/scrape-goat.py
+++ src/scrape-goat.py
@@ -5,12 +5,8 @@ from browser.selenium import ScrapeGoatBrowser
 from routines.simple_login import SimpleLoginRoutine
 
 config = getConfig()
-print(config.keys())
-
-quit()
 
 customBrowser = ScrapeGoatBrowser('Chrome');
-
 
 SimpleLoginRoutine(customBrowser, True)
 input()
